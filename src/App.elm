77f444967edbad18fb8a1e8b12a2149163d6824f
Isaac Ross <isaac253@gmail.com>
2017-01-06 20:01:19 -0500

    initial commit

---
+++ src/App.elm
@@ -0,0 +1,49 @@
+module App exposing (..)
+
+import Html exposing (Html, text, div, node)
+import Html.Attributes exposing (attribute, style)
+
+
+type alias Model =
+    { message : String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { message = "Your Elm App is working!" }, Cmd.none )
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text model.message
+        , node "paper-input"
+            [ attribute "label" "Username" ]
+            []
+        , div
+            []
+            [ node "paper-button"
+                [ attribute "raised" "raised"
+                , style
+                    [ ( "background", "#1E88E5" )
+                    , ( "color", "white" )
+                    ]
+                ]
+                [ text "Clicky" ]
+            ]
+        ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
